0110703686d22cf4b8873d7eff3ddd6e8c6dcbaa
pablo.moes15 <pablo.moes15@gmail.com>
2022-05-30 14:08:49 -0500

    Panel

--- config/conn.php
+++ config/conn.php
@@ -1,14 +1,15 @@
 <?php
 // Conexion a la base de datos
-	/* $server = 'localhost';
-	$database = 'panel_madigen';
-	$username = 'root';
-	$password = ''; 
-	*/
+$server = 'localhost';
+$database = 'panel_madigen';
+$username = 'root';
+$password = ''; 
+/* 
 	$server = 'localhost';
 	$database = 'madigenm_panel_blogs';
 	$username = 'madigenm_blogs';
 	$password = 'aIdRbof#EE*m'; 
+	*/
 
 	$conn = new mysqli($server, $username, $password, $database);
 	if($conn->connect_errno){
